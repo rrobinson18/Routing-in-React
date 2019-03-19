945f86a1fc6bdbd4c5159e31d045c62592de28ad
rrobinson18 <robinson.rakesha@gmail.com>
2019-03-19 10:58:20 -0400

    step one

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component, Fragment} from 'react';
 import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 
-class App extends React.Component {
+
+class App extends Component  {
     render() {
         return(
             <Router>
                 <Fragment>
-                    <Link></Link>
-                    <Link></Link>
+                    <Link className="btn btn-primary">Go Home</Link>
+                    <Link className="btn btn-primary">View Films</Link>
+                    <Link className="btn btn-primary">View People</Link>
                     <Switch>
                     <Route />
                     <Route />
